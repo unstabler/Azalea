fe8f02221f62d073c2ca50909c59f9bd7b74f248
Gyuhwan Park★ <doping.cheese@gmail.com>
2019-02-10 02:02:44 +0900

    feature(serialization): toJSON, Array 관련 조작 정의

--- azalea/serialization.hpp
+++ azalea/serialization.hpp
@@ -1,9 +1,39 @@
 #pragma once
 
+#include <QDebug>
 #include <QJsonObject>
+#include <QJsonArray>
+
+template <typename T>
+inline QJsonObject toJSON(T *source)
+{
+    QJsonObject destination;
+    toJSON(source, destination);
+    
+    return destination;
+}
+
+template <typename T>
+void toJSON(T *source, QJsonObject &destination);
 
 template <typename T>
 void fromJSON(T *destination, QJsonObject source);
+
+namespace serialization {
+    template<typename TIterator>
+    inline QJsonArray TO_JSONARRAY(TIterator first, TIterator last)
+    {
+        QJsonArray array;
+        
+        while (first != last) {
+            auto value = *first;
+            array.push_back(toJSON(value));
+            first++;
+        }
+        
+        return array;
+    }
+}
 
 /**
  */
@@ -32,12 +62,50 @@ namespace deserialization {
         Q_ASSERT(valueRef.isNull() || valueRef.isObject());
         return valueRef.toObject();
     }
+    
+    inline QJsonArray JSONARRAY(const QJsonValueRef &valueRef)
+    {
+        Q_ASSERT(valueRef.isNull() || valueRef.isArray());
+        return valueRef.toArray();
+    }
+    
+    template <typename T>
+    inline T* OBJECT(const QJsonObject &value)
+    {
+        T* destination = new T;
+        fromJSON(destination, value);
+        return destination;
+    }
 
     template <typename T>
     inline T* OBJECT(const QJsonValueRef &valueRef)
     {
-        T* destination = new T;
-        fromJSON(destination, JSONOBJECT(valueRef));
+        QJsonObject value = JSONOBJECT(valueRef);
+        return OBJECT<T>(value);
+    }
+    
+    template <typename T, class TOutputIterator>
+    inline void TRANSFORM_ARRAY(const QJsonArray &array, TOutputIterator tBegin)
+    {
+        std::transform(
+                array.begin(),
+                array.end(),
+                tBegin,
+                [](const QJsonValue &jsonValue) {
+                    return OBJECT<T>(jsonValue.toObject());
+                }
+        );
+    }
+    
+    template <typename T>
+    inline QList<T*>* ARRAY(const QJsonValueRef &valueRef)
+    {
+        auto *destination = new QList<T*>;
+        TRANSFORM_ARRAY<T, std::back_insert_iterator<QList<T*>>>(
+                JSONARRAY(valueRef),
+                // FIXME: destination->begin() 쓰면 아무런 값도 들어가지 않는데 그 이유는 무엇일까요 . .)
+                std::back_inserter(*destination)
+        );
         return destination;
     }
 
