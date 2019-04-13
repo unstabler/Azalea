7a21d32b0a59a4b73fb36aefe9e8da266cc84776
Gyuhwan Park <doping.cheese@gmail.com>
2019-04-13 16:55:12 +0900

    refactor: protobuf 의존 제거

--- azalea/main.cpp
+++ azalea/main.cpp
@@ -3,16 +3,10 @@
 #include "azaleaapplication.hpp"
 #include "singleton.hpp"
 
-#include "google/protobuf/stubs/common.h"
-
 int main(int argc, char *argv[])
 {
-    GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-    AzaleaApplication* app = new AzaleaApplication(argc, argv);
+    auto* app = new AzaleaApplication(argc, argv);
     const auto returnValue = app->exec();
-
-    google::protobuf::ShutdownProtobufLibrary();
 
     SingletonFinalizer::finalize();
     return returnValue;
