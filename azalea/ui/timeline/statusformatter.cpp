841cf2738ad336e19073874768b20de24a074b5b
Gyuhwan Park <doping.cheese@gmail.com>
2019-06-16 14:52:49 +0900

    feature(StatusFormatter): emoji shortcode -> html richtext로 변환

--- azalea/ui/timeline/statusformatter.cpp
+++ azalea/ui/timeline/statusformatter.cpp
@@ -11,9 +11,17 @@ StatusFormatter::StatusFormatter()
 
 QString StatusFormatter::toRichText(v1::Status *status)
 {
-    return "";
+    QString content = status->content;
+    for (auto emoji : status->emojis) {
+        // 진짜 꼭 이래야 하나
+        const QString html = "<img src=\"%1\" width=\"20\" height=\"20\">";
+        content.replace(":" + emoji->shortcode + ":", html.arg(emoji->staticUrl.toString()));
+    }
+    
+    return content;
 }
 
+// 이거 필요 없어진 거 아냐..?
 QSharedPointer<v1::Emoji> StatusFormatter::resolveEmoji(v1::Status *status, const QString shortcode)
 {
     auto iterator = std::find_if(
