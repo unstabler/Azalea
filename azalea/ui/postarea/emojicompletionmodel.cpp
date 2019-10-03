5d8e4fc1544a0a14b39f11ec712ea54051f520ac
Gyuhwan Park <doping.cheese@gmail.com>
2019-10-03 16:34:41 +0900

    feature(EmojiCompletionModel): emoji 자동 완성을 위한 모델 클래스 정의

---
+++ azalea/ui/postarea/emojicompletionmodel.cpp
@@ -0,0 +1,42 @@
+//
+// Created by cheesekun on 2019/10/03.
+//
+
+#include "emojicompletionmodel.hpp"
+
+const int EmojiCompletionModel::COLUMN_INDEX_SHORTCODE = 0;
+const int EmojiCompletionModel::COLUMN_INDEX_EMOJI_URL = 1;
+
+EmojiCompletionModel::EmojiCompletionModel(QSharedPointer<ResourceList<v1::Emoji>> emojiList) :
+    _emojiList(std::move(emojiList))
+{
+
+}
+
+int EmojiCompletionModel::rowCount(const QModelIndex &parent) const
+{
+    return _emojiList->length();
+}
+
+int EmojiCompletionModel::columnCount(const QModelIndex &parent) const
+{
+    return 2;
+}
+
+QVariant EmojiCompletionModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        return QVariant();
+    }
+    
+    auto emoji = _emojiList->at(index.row());
+    if (index.column() == COLUMN_INDEX_SHORTCODE) {
+        return emoji->shortcode;
+    } else if (index.column() == COLUMN_INDEX_EMOJI_URL) {
+        return emoji->staticUrl;
+    }
+    
+    return QVariant();
+}
+
+
